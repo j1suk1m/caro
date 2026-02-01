f2a84827eacc195702ebf0272a959e8854401255
jisukimtech <jisukim.tech@gmail.com>
2026-02-01 22:01:15 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/today/caro/api/CaroApplication.java
@@ -0,0 +1,13 @@
+package today.caro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/today/caro/api/CaroApplicationTests.java
@@ -0,0 +1,13 @@
+package today.caro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
