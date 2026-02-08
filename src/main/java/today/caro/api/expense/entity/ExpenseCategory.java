4a9e9c4de7d44329c2d78fc95c77656b0e291610
jisukimtech <jisukim.tech@gmail.com>
2026-02-08 12:49:21 +0900

    feat: 지출 카테고리에 세차비 추가

--- src/main/java/today/caro/api/expense/entity/ExpenseCategory.java
+++ src/main/java/today/caro/api/expense/entity/ExpenseCategory.java
@@ -10,7 +10,8 @@ public enum ExpenseCategory {
     FUEL("주유비"),
     MAINTENANCE("정비·수리비"),
     PARKING("주차비"),
-    TOLL("통행료");
+    TOLL("통행료"),
+    CAR_WASH("세차비");
 
     private final String description;
 
